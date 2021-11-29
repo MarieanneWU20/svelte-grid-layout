e2838e9fc49d4fad38cc3193b8788d8336518a9a
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-11-29 21:47:43 +0100

    played around with proof of concept

--- example/src/main.js
+++ example/src/main.js
@@ -1,16 +1,13 @@
 // const test = require("../node_modules/svelte-layout-editor/index.js");
-import  {foobar}  from "../node_modules/svelte-layout-editor/index.js";
+import LayoutEditor  from "../node_modules/svelte-layout-editor/src/LayoutEditor.svelte";
 
 // console.log(test.foobar());
 //  const root = document.getElementById('root');
 
-//  root.innerHTML = test.foobar();
- import App from './App.svelte';
-
-const app = new App({
+const app = new LayoutEditor({
 	target: document.body,
 	props: {
-		name: foobar()
+		name: 'WORLD'
 	}
 });
 
