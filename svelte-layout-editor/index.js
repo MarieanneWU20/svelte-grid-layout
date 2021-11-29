1be9920b33858f44f110e89052cec6993b6659fe
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-11-29 13:37:27 +0100

    link tested and ready

--- svelte-layout-editor/index.js
+++ svelte-layout-editor/index.js
@@ -1,5 +1,2 @@
-const foobar = () => {
-   return "Hello World"};
-module.test = {foobar: foobar}; 
-// console.log(foobar);
-// console.log(module.test);
+
+exports.foobar = () => { return "Hello from svelte-layout-editor!" };
