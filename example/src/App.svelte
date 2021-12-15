3f9c4b4388bfdbbe879fe89688e7e78555a0ea7f
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-12-15 11:39:41 +0100

    altered props values

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -11,9 +11,14 @@
             <Column height=1 width=1><p>Test 3 från App</p></Column>
         </Row>
         <Row>
-            <Column height=2 width=1><p>Test 4 från App</p></Column>
-            <Column height=2 width=1><p>Test 5 från App</p></Column>
-            <Column height=2 width=1><p>Test 6 från App</p></Column>
+            <Column height=1 width=1><p>Test 4 från App</p></Column>
+            <Column height=1 width=1><p>Test 5 från App</p></Column>
+            <Column height=1 width=1><p>Test 6 från App</p></Column>
+        </Row>
+        <Row>
+            <Column height=1 width=1><p>Test 7 från App</p></Column>
+            <Column height=1 width=1><p>Test 8 från App</p></Column>
+            <Column height=1 width=1><p>Test 9 från App</p></Column>
         </Row>
         <!-- <Row height=1></Row> -->
     </Grid>
