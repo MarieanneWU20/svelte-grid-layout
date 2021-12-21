bbf487717ada755e4d4affb14255ac09c3a0afa6
Marie <kerstinmarie@hotmail.com>
2021-12-21 12:43:35 +0100

    Removed doubble style and padding

--- svelte-grid-layout/src/components/Column.svelte
+++ svelte-grid-layout/src/components/Column.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { getContext } from 'svelte';
+	import { getContext } from "svelte";
 
 	export let width;
 	export let height;
@@ -10,71 +10,60 @@
 		let run = true;
 		while (run) {
 			let endColumn = startColumn + width;
-			let endRow = startRow+h+1;
+			let endRow = startRow + h + 1;
 			run = false;
-			for(let block of grid.blocks) {
-				
-				if ( startRow+1 > block.startRow && block.endRow > startRow ) {
-						y = block.endRow - 1;
-				};
-			};
-		};
+			for (let block of grid.blocks) {
+				if (startRow + 1 > block.startRow && block.endRow > startRow) {
+					y = block.endRow - 1;
+				}
+			}
+		}
 		return startColumn;
 	};
 
 	let w = Number(width);
 	let h = Number(height);
 	let y = Number(grid.rowIndex);
-	let x = checkX(Number(grid.columnIndex), y, w,h);
-	const end = x+w;
+	let x = checkX(Number(grid.columnIndex), y, w, h);
+	const end = x + w;
 
 	grid.columnIndex = grid.columnIndex = end;
 
 	if (h > 1) {
-		for(let nextRow=1; nextRow<h; nextRow++) {
-			const block = { 
-				startRow: y+nextRow,
-				endRow: y+nextRow+h,
+		for (let nextRow = 1; nextRow < h; nextRow++) {
+			const block = {
+				startRow: y + nextRow,
+				endRow: y + nextRow + h,
 				startColumn: x,
-				endColumn: x+w-1
-			 }
-			 grid.blocks.push(block);
+				endColumn: x + w - 1,
+			};
+			grid.blocks.push(block);
 		}
 	}
 </script>
 
-<div class="section" style="grid-area: { `${y + 1}/${x + 1}/${y + 1 + h}/${x + 1 + w}` } ;">
+<div
+	class="section" style="grid-area: {`${y + 1}/${x + 1}/${y + 1 + h}/${x + 1 + w}`} ;">
 	<slot />
 </div>
 
 <style>
-    .section {
-		 width: auto;
-		 height: auto;
-		 border-radius: 20px;
-		 background-color: #ffffff;
-		 text-align: center;
-		 vertical-align: middle;
-	}
 	.section {
-		 width: auto;
-		 height: auto;
-		 border-radius: 20px;
-		 background-color: #ffffff;
-		 text-align: center;
-		 vertical-align: middle;
-	 }
-	 @media screen and (min-width: 480px) {
-
-	  .section{
-		 border-radius: 25px;
-		 padding: 15px;
-	 }
+		width: auto;
+		height: auto;
+		border-radius: 20px;
+		background-color: #ffffff;
+		text-align: center;
+		vertical-align: middle;
+	}
+	@media screen and (min-width: 480px) {
+		.section {
+			border-radius: 25px;
+		}
 	}
 	@media screen and (min-width: 768px) {
-	  .section{
-		 border-radius: 30px;
-		 padding: 20px;
-		 }
+		.section {
+			border-radius: 30px;
+		}
 	}
 </style>
